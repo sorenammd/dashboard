a5bea778110a0aa6c254bf0c82d87e4b35f173cf
sorenammd <mohammad01293@gmail.com>
2022-12-13 21:31:28 +0330

    add darkmode

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React,{useState} from 'react'
-import { LightModeOutlined,DarkMode,Menu as MenuIcon, Search,SettingsOutlined,ArrowDropDownOutlined } from '@mui/icons-material'
+import { LightModeOutlined,DarkMode,Menu as MenuIcon, Search,SettingsOutlined,ArrowDropDownOutlined, DarkModeOutlined } from '@mui/icons-material'
 import FlexBetween from './FlexBetween'
 import { useDispatch } from 'react-redux'
 import { setMode } from 'state'
@@ -32,6 +32,19 @@ const Navbar = () => {
                         <Search/>
                     </IconButton>
                     </FlexBetween>
+            </FlexBetween>
+            <FlexBetween gap='1.5rem'>
+                <IconButton onClick={()=> dispatch(setMode())}>
+                    {theme.palette.mode === 'dark' ? (
+                        <DarkModeOutlined sx={{fontSize:"25px"}}/>
+                    ):(
+                        <LightModeOutlined sx={{fontSize:"25px"}}/>
+                  
+                    )}
+                </IconButton>
+                <IconButton>
+                    <SettingsOutlined  sx={{fontSize:"25px"}}/>
+                </IconButton>
             </FlexBetween>
         </Toolbar>
     </AppBar>
