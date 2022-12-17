5e577faef9350295423c62187512e8daba52261d
sorenammd <mohammad01293@gmail.com>
2022-12-17 10:59:01 +0330

    add Header component and import to products

---
+++ client/src/scenes/products/index.jsx
@@ -0,0 +1,13 @@
+import React,{useState} from 'react'
+import { Box,Card,CardActions,CardContent,Collapse,Button,Typography,Rating,useTheme,useMediaQuery } from '@mui/material'
+import { useGetProductsQuery } from 'state/api'
+import Header from "components/Header"
+const Products = () => {
+  return (
+    <Box>
+        <Header title="Products" subtitle="see your list of products" />
+    </Box>
+  )
+}
+
+export default Products
