a4d4f9ef0d029081ca1c19ef21a261bbef9aa8c8
mohammad mohammadi <dev.mohammadii@gmail.com>
2022-12-22 12:36:27 +0330

    add dashboard page

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -8,6 +8,7 @@ import Breakdown from 'scenes/Breakdown'
 import OverviewChart from 'components/OverviewChart'
 import { useGetDashboardQuery } from 'state/api'
 import StatBox from "components/StatBox"
+import BreakdownChart from 'components/BeakdownChart'
 const Dashboard = () => {
     const theme = useTheme();
     const isNonMediumScreens = useMediaQuery("(min-width:1200px)");
@@ -84,7 +85,7 @@ const Dashboard = () => {
           }}/>}/>
           <StatBox
           title="Sales Today"
-          value={data && data.todayStats.totalCustomers}
+          value={data && data.todayStats.totalSales}
           increase="+21%"
           description="Since last month"
           icon={<PointOfSale sx={{
@@ -109,14 +110,71 @@ const Dashboard = () => {
           }}/>}/> 
           <StatBox
           title="Yearly Sales"
-          value={data && data.yealySalesTotal}
+          value={data && data.yearlySalesTotal}
           increase="+43%"
           description="Since last month"
           icon={<Traffic sx={{
             color:theme.palette.secondary[300] ,fontSize:"26px"
           }}/>}/>
-    </Box>
 
+
+        <Box
+          gridColumn="span 8"
+          gridRow="span 3"
+          sx={{
+            "& .MuiDataGrid-root": {
+              border: "none",
+              borderRadius: "5rem",
+            },
+            "& .MuiDataGrid-cell": {
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: theme.palette.background.alt,
+              color: theme.palette.secondary[100],
+              borderBottom: "none",
+            },
+            "& .MuiDataGrid-virtualScroller": {
+              backgroundColor: theme.palette.background.alt,
+            },
+            "& .MuiDataGrid-footerContainer": {
+              backgroundColor: theme.palette.background.alt,
+              color: theme.palette.secondary[100],
+              borderTop: "none",
+            },
+            "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+              color: `${theme.palette.secondary[200]} !important`,
+            },
+          }}
+        >
+            <DataGrid
+            loading={isLoading || !data}
+            getRowId={(row) => row._id}
+            rows={(data && data.transactions) || []}
+            columns={columns}
+          />
+          </Box>
+          <Box
+          gridColumn="span 4"
+          gridRow="span 3"
+          backgroundColor={theme.palette.background.alt}
+          p="1.5rem"
+          borderRadius="0.55rem"
+        >
+          <Typography variant="h6" sx={{ color: theme.palette.secondary[100] }}>
+            Sales By Category
+          </Typography>
+          <BreakdownChart isDashboard={true} />
+          <Typography
+            p="0 0.6rem"
+            fontSize="0.8rem"
+            sx={{ color: theme.palette.secondary[200] }}
+          >
+            Breakdown of real states and information via category for revenue
+            made for this year and total sales.
+          </Typography>
+        </Box>
+      </Box>
     </Box>
   )
 }
